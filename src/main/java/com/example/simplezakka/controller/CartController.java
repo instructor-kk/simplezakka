20aef5fd442941a684f92414a23568077fe5d301
instructor-kk <kuroko.keiko@gmail.com>
2025-06-26 14:22:30 +0900

    Controllerにクロスオリジンを追加

--- src/main/java/com/example/simplezakka/controller/CartController.java
+++ src/main/java/com/example/simplezakka/controller/CartController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "*")    // 06/26追加
 @RestController
 @RequestMapping("/api/cart")
 public class CartController {
--- src/main/java/com/example/simplezakka/controller/OrderController.java
+++ src/main/java/com/example/simplezakka/controller/OrderController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "*")    // 06/26追加
 @RestController
 @RequestMapping("/api/orders")
 public class OrderController {
